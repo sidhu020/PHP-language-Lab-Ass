bad482d6e78fb842724778052228f376e3066012
= <=>
2023-01-19 12:41:40 +0530

    q5 added

---
+++ Q6.php
@@ -0,0 +1,23 @@
+<!-- 6. Hit For example, number 24142 is a palindrome number. On reversing it we?ll get the same number
+ -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+
+
+?>
+
+</body>
+</html>
