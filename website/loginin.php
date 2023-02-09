d72bb6eb6401b0daf9b29a91917473d853396c66
BORISAGAR SIDDHARTH <115538682+sidhu020@users.noreply.github.com>
2023-02-09 16:44:05 +0530

    website 1

---
+++ website/loginin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body class="stage">
+   <div class="center">
+       <h1>Login</h1> 
+        <form>
+        <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+              </div><br>
+
+              <button type="submit" class="btn btn-outline-success">Submit</button>
+
+           <button type="reset" class="btn btn-outline-danger" class="">Reset</button>
+
+
+
+       </form>
+   </div>
+</body>
+</html>
